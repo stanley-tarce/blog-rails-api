33b1c1a8a1ea033d2cac30300565cce12047ef88
stanley-tarce <stanleytarce18@gmail.com>
2022-02-08 15:43:39 +0800

    Removed Uniqueness in Categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   before_create :slugify
   belongs_to :user
   has_many :tasks, dependent: :destroy
-  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   def slugify
     self.slug = name.parameterize
   end
