120be9a6104121756bb25f34d8555ed1f0a8ffa0
stanley-tarce <stanleytarce18@gmail.com>
2022-02-08 15:38:14 +0800

    Added Changes in Category.rb Added dependent destroy to mitigate 500 error

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   before_create :slugify
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
   def slugify
     self.slug = name.parameterize
